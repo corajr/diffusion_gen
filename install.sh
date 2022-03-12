d6efc7afac8458a38cc5b1f8bb52bee352ac7705
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2022-03-12 00:04:57 -0500

    combine package installs

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 source ${CONDA_ROOT:-~/miniconda3}/etc/profile.d/conda.sh
 conda activate diffusion_gen
-git clone https://github.com/CompVis/latent-diffusion.git && \
-       git clone https://github.com/openai/CLIP && \
-       pip3 install -e ./CLIP && \
-       git clone https://github.com/assafshocher/ResizeRight.git && \
-       git clone https://github.com/crowsonkb/guided-diffusion && \
-       python3 -m pip install -e ./guided-diffusion && \
-       git clone https://github.com/CompVis/latent-diffusion.git && \
-       git clone https://github.com/CompVis/taming-transformers && \
-       pip install -e ./taming-transformers
+
+python3 -m pip install torch lpips datetime timm ipywidgets omegaconf>=2.0.0 pytorch-lightning>=1.0.8 torch-fidelity einops wandb
+git clone https://github.com/CompVis/latent-diffusion.git
+git clone https://github.com/openai/CLIP
+pip3 install -e ./CLIP
+git clone https://github.com/assafshocher/ResizeRight.git
+git clone https://github.com/crowsonkb/guided-diffusion
+python3 -m pip install -e ./guided-diffusion
+git clone https://github.com/CompVis/latent-diffusion.git
+git clone https://github.com/CompVis/taming-transformers
+pip install -e ./taming-transformers
